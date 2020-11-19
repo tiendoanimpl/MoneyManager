a5a7b84c9c90b38f2e1d12b9fa5be2e008a1479f
tien.doan <tien.doan@impl.vn>
2020-11-19 18:42:20 +0700

    Format code

--- src/components/NavigationDrawer/index.js
+++ src/components/NavigationDrawer/index.js
@@ -1,1 +1,1 @@
-export {default} from './NavigationDrawer';
+export { default } from './NavigationDrawer';
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
-  return <Text style={{marginTop: 100}}>Home</Text>;
+const HomeScreen = ({ navigation }) => {
+  return <Text style={{ marginTop: 100 }}>Home</Text>;
 };
 
 export default HomeScreen;
